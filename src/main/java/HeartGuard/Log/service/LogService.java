a24a38b18447bda74e8796e443522e41e7a1d77d
2ãmxnxu <leeeun099@naver.com>
2025-04-24 18:21:26 +0900

    feat 04/24 이민수 LogController, LogService 파일생성

---
+++ src/main/java/HeartGuard/Log/service/LogService.java
@@ -0,0 +1,12 @@
+package HeartGuard.Log.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LogService {
+
+}
