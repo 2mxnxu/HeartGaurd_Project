2ad4f16c58f1aa17e063b55c29de1b05c041cd35
2ãmxnxu <leeeun099@naver.com>
2025-05-02 12:11:49 +0900

    fix 5/1 이민수 db수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,31 @@
 -- 병원 데이터
-INSERT INTO hospital(hid, hpwd, apino ) VALUES ('hospital1', '1234', 3901); -- 의료법인 길의료재단 길병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital2', '1234', 2310); -- 인하대학교의과대학부속병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital3', '1234', 6234); -- 가톨릭관동대학교인천성모병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital4', '1234', 2532); -- 가톨릭관동대학교국제성모병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital5', '1234', 0366); -- 검단탑병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital6', '1234', 8135); -- 인천세종병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital7', '1234', 9396); -- 의료법인 나사렛의료재단 나사렛국제병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital8', '1234', 7782); -- 의료법인 루가의료재단 나은병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital9', '1234', 2924); -- 인천사랑병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital10', '1234', 5460); -- 안은의료재단부평세림병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital11', '1234', 8670); -- 한림병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital12', '1234', 0008); -- 비에스종합병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital13', '1234', 5555); -- 온누리병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital14', '1234', 1072); -- 의료법인 성세의료재단 뉴 성민병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital15', '1234', 6561); -- 인천광역시의료원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital16', '1234', 8410); -- 인천기독병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital17', '1234', 5698); -- 현대유비스병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital18', '1234', 1733); -- 인천광역시의료원백령병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital19', '1234', 3169); -- 인천힘찬종합병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital20', '1234', 4009); -- 인천적십자병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital21', '1234', 8118); -- 인천백병원
+INSERT INTO hospital(hid, hpwd, apino ) VALUES ('hospital1', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 3901); -- 의료법인 길의료재단 길병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital2', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2310); -- 인하대학교의과대학부속병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital3', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 6234); -- 가톨릭관동대학교인천성모병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital4', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2532); -- 가톨릭관동대학교국제성모병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital5', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 0366); -- 검단탑병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital6', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8135); -- 인천세종병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital7', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 9396); -- 의료법인 나사렛의료재단 나사렛국제병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital8', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 7782); -- 의료법인 루가의료재단 나은병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital9', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2924); -- 인천사랑병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital10', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5460); -- 안은의료재단부평세림병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital11', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8670); -- 한림병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital12', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 0008); -- 비에스종합병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital13', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5555); -- 온누리병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital14', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 1072); -- 의료법인 성세의료재단 뉴 성민병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital15', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 6561); -- 인천광역시의료원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital16', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8410); -- 인천기독병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital17', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5698); -- 현대유비스병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital18', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 1733); -- 인천광역시의료원백령병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital19', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 3169); -- 인천힘찬종합병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital20', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 4009); -- 인천적십자병원
+INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital21', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8118); -- 인천백병원
 -- 사용자 데이터
-INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('admin', 'admin', '관리자', 1, '010-2222-2222');
-INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('abc123', '1234', '이민수', 0, '010-2222-2223');
-INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('1111', '1111', '장민우', 0, '010-2222-2225');
-INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('jeon', 'jeon', '전은서', 0, '010-2222-2226');
-INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('bear', '0305', '류예나', 0, '010-2222-2227');
+INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('admin', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '관리자', 1, '010-2222-2222');
+INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('abc123', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '이민수', 0, '010-2222-2223');
+INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('1111', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '장민우', 0, '010-2222-2225');
+INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('jeon', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '전은서', 0, '010-2222-2226');
+INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('bear', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '류예나', 0, '010-2222-2227');
 
 -- 1. category (카테고리 샘플 데이터)
 insert into category (cname) values
