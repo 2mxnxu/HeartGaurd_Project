a8eaa758f0f112f3424a23038c362480b84c3f06
ithodol <ddmdjesz1@gmail.com>
2025-05-08 12:56:43 +0900

    5/8 feat 전은서 병원 데이터 위도, 경도 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,25 @@
 -- 병원 데이터
-INSERT INTO hospital(hid, hpwd, apino ) VALUES ('hospital1', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 3901); -- 의료법인 길의료재단 길병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital2', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2310); -- 인하대학교의과대학부속병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital3', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 6234); -- 가톨릭관동대학교인천성모병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital4', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2532); -- 가톨릭관동대학교국제성모병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital5', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 0366); -- 검단탑병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital6', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8135); -- 인천세종병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital7', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 9396); -- 의료법인 나사렛의료재단 나사렛국제병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital8', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 7782); -- 의료법인 루가의료재단 나은병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital9', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2924); -- 인천사랑병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital10', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5460); -- 안은의료재단부평세림병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital11', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8670); -- 한림병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital12', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 0008); -- 비에스종합병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital13', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5555); -- 온누리병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital14', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 1072); -- 의료법인 성세의료재단 뉴 성민병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital15', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 6561); -- 인천광역시의료원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital16', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8410); -- 인천기독병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital17', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5698); -- 현대유비스병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital18', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 1733); -- 인천광역시의료원백령병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital19', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 3169); -- 인천힘찬종합병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital20', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 4009); -- 인천적십자병원
-INSERT INTO hospital(hid, hpwd, apino) VALUES ('hospital21', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8118); -- 인천백병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital1', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 3901, 37.4521710, 126.7092258); -- 의료법인 길의료재단 길병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital2', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2310, 37.4586786, 126.6340954); -- 인하대학교의과대학부속병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital3', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 6234, 37.4848309, 126.7248987); -- 가톨릭관동대학교인천성모병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital4', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2532, 37.5424164, 126.6836102); -- 가톨릭관동대학교국제성모병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital5', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 0366, 37.5924835, 126.6711791); -- 검단탑병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital6', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8135, 37.5324735, 126.7368717); -- 인천세종병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital7', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 9396, 37.4079580, 126.6704928); -- 의료법인 나사렛의료재단 나사렛국제병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital8', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 7782, 37.4890717, 126.6797292); -- 의료법인 루가의료재단 나은병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital9', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 2924, 37.4622040, 126.6807094); -- 인천사랑병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital10', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5460, 37.5068719, 126.7199857); -- 안은의료재단부평세림병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital11', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8670, 37.5320981, 126.7343513); -- 한림병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital12', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 0008, 37.7360478, 126.4859924); -- 비에스종합병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital13', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5555, 37.6027927, 126.6543363); -- 온누리병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital14', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 1072, 37.5092080, 126.6697732); -- 의료법인 성세의료재단 뉴 성민병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital15', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 6561, 37.4787429, 126.6687500); -- 인천광역시의료원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital16', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8410, 37.4712156, 126.6321218); -- 인천기독병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital17', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 5698, 37.4615989, 126.6429995); -- 현대유비스병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital18', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 1733, 37.9680090, 124.7210341); -- 인천광역시의료원백령병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital19', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 3169, 37.4011109, 126.7147129); -- 인천힘찬종합병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital20', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 4009, 37.4184945, 126.6896544); -- 인천적십자병원
+INSERT INTO hospital(hid, hpwd, apino, hlat, hlong) VALUES ('hospital21', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', 8118, 37.4817037, 126.6458376); -- 인천백병원
 -- 사용자 데이터
 INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('admin', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '관리자', 1, '010-2222-2222');
 INSERT INTO user(uid, upwd, uname, ustate, uphone) VALUES ('abc123', '$2a$10$ces9K/Dj7MUvZT4z5Z53yent9GqwfzsEhGuPNA9Ct6NzXq6ZHEXw.', '이민수', 0, '010-2222-2223');
@@ -45,10 +45,10 @@ insert into ireply (rcontent, uno, bno,create_at) values
 
 -- 4. hlog (병원 방문 기록, uno=1,2,3,4번 사용자, hno=1~4로 가정)
 insert into hlog (llat, llong, lstate, phone, hno,create_at) values
-('37.448894', '126.702515', 1, "010-1234-4567", 1,NOW()),  -- 길병원
-('37.456486', '126.633448', 1, "010-1233-3456", 2,NOW()),  -- 인하대병원
-('37.474467', '126.642024', 1, "010-1234-5678", 3,NOW()),  -- 인천성모병원
-('37.518291', '126.719215', 1, "010-1234-5679", 4,NOW());  -- 국제성모병원
+('37.448894', '126.702515', 1, "010-1234-4567", 1,NOW());  -- 길병원
+--('37.456486', '126.633448', 1, "010-1233-3456", 2,NOW()),  -- 인하대병원
+--('37.474467', '126.642024', 1, "010-1234-5678", 3,NOW()),  -- 인천성모병원
+--('37.518291', '126.719215', 1, "010-1234-5679", 4,NOW());  -- 국제성모병원
 
 INSERT INTO img (create_at,iname, bno) VALUES
 (NOW(),'AED.JPG', 1),(NOW(),'CPR.JPG',2);
