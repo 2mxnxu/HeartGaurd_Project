7b9b797ef5dec8d25bfc46e4c867a4417b1df536
ithodol <ddmdjesz1@gmail.com>
2025-05-08 13:19:19 +0900

    5/8 feat 전은서 병원 자료 가져오기 진행 중

---
+++ src/main/java/HeartGuard/Map/controller/MapController.java
@@ -0,0 +1,23 @@
+package HeartGuard.Map.controller;
+
+import HeartGuard.Hospital.model.dto.HospitalDto;
+import HeartGuard.Map.service.MapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/map")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class MapController {
+
+    private final MapService mapService;
+
+//    @GetMapping("/gethospital")
+//    public ResponseEntity<List<HospitalDto>> getHospital(@RequestBody HospitalDto hospitalDto){
+//        return mapService.getHospital(hospitalDto);
+//    }
+}
