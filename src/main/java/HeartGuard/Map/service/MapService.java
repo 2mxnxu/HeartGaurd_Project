f43a1bf6348a55449cec257f494816806ef3a850
ithodol <ddmdjesz1@gmail.com>
2025-05-08 16:14:17 +0900

    5/8 feat 전은서 병원 자료 가져오기 완료

--- src/main/java/HeartGuard/Map/service/MapService.java
+++ src/main/java/HeartGuard/Map/service/MapService.java
@@ -1,6 +1,7 @@
 package HeartGuard.Map.service;
 
 import HeartGuard.Hospital.model.dto.HospitalDto;
+import HeartGuard.Hospital.model.entity.HospitalEntity;
 import HeartGuard.Hospital.model.repository.HospitalEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MapService {
     private final HospitalEntityRepository hospitalEntityRepository;
 
-//    @GetMapping("/gethospital")
-//    public ResponseEntity<List<HospitalDto>> getHospital(HospitalDto hospitalDto){
-//        return hospitalEntityRepository.getHospital();
-//    }
+    public ResponseEntity<List<HospitalDto>> getHospital() {
+        List<HospitalDto> hospitalDtoList = hospitalEntityRepository.findAll()
+                .stream()
+                .map(HospitalEntity::toDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(hospitalDtoList);
+    }
 }
